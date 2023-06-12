65dec62c8031f72438d5df218fa4aa9a5d522a4e
Code Evangelis☦︎ <58617938+codevangelist@users.noreply.github.com>
2023-06-12 20:36:35 +0000

    dotenv

--- src/app/helper.ts
+++ src/app/helper.ts
@@ -1,65 +1,55 @@
 class AudioTranscript {
-    constructor() {
-        
-    }
-    async AssemblyAI(audioFile: any) => {
-        const upload = () => {
-            const data = {
-                "audio_url" : audioFile
-            };
-            const params = {
-                headers:{
-                    "authorization": process.env.ASSEMBLYAI_API_KEY,
-                    "content-type": "application/json",
-                },
-                body: JSON.stringify(data),
-                method: "POST"
-            };
-            fetch(url, params)
-                .then(response => response.json())
-                .then(data => {
-                    console.log('Success:', data);
-                    return data['id'];
-                })
-                .catch((error) => {
-                    console.error('Error:', error);
-                });
-        }
-
-        const download = (id: string) => {
-            const url = `https://api.assemblyai.com/v2/transcript/${id}`;
-            const params = {
-                headers: {
-                  "authorization": process.env.ASSEMBLYAI_API_KEY,
-                  "content-type": "application/json",
-                }, 
-                method: "GET"
-            };
-
-            fetch(url, params)
-                .then(response => response.json())
-                .then(data => {
-                    print(data);
-                })
-                .catch((error) => {
-                    console.error(`Error: ${error}`);
-                });
-        }
-    }
-
-    private print(data: any) {
-        switch (data.status) {
+    async transcribe(audioFile: any) {
+      const uploadUrl = 'https://api.assemblyai.com/v2/transcript';
+      const uploadParams = {
+        headers: {
+          'authorization': process.env.ASSEMBLYAI_API_KEY ? process.env.ASSEMBLYAI_API_KEY : '',
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({ 'audio_url': audioFile }),
+        method: 'POST',
+      };
+  
+      try {
+        const uploadResponse = await fetch(uploadUrl, uploadParams);
+        const uploadData = await uploadResponse.json();
+        const transcriptId = uploadData['id'];
+  
+        const transcriptUrl = `https://api.assemblyai.com/v2/transcript/${transcriptId}`;
+        const transcriptParams = {
+          headers: {
+            'authorization': process.env.ASSEMBLYAI_API_KEY ? process.env.ASSEMBLYAI_API_KEY : '',
+            'content-type': 'application/json',
+          },
+          method: 'GET',
+        };
+  
+        let isTranscribing = true;
+        while (isTranscribing) {
+          const transcriptResponse = await fetch(transcriptUrl, transcriptParams);
+          const transcriptData = await transcriptResponse.json();
+  
+          switch (transcriptData.status) {
             case 'queued':
             case 'processing':
               console.log('AssemblyAI is still transcribing your audio, please try again in a few minutes!');
+              await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait for 5 seconds before checking again
               break;
             case 'completed':
-              console.log(`Success: ${data}`);
-              console.log(`Text: ${data.text}`);
+              console.log(`Success: ${JSON.stringify(transcriptData)}`);
+              console.log(`Text: ${transcriptData.text}`);
+              isTranscribing = false;
               break;
             default:
-              console.log(`Something went wrong :-( : ${data.status}`);
+              console.log(`Something went wrong :-( : ${transcriptData.status}`);
+              isTranscribing = false;
               break;
+          }
         }
+      } catch (error) {
+        console.error(`Error: ${error}`);
+      }
     }
 }
+
+export default AudioTranscript
